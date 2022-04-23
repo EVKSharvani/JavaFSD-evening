92ab1473c145d38d515d70272f1ba33ddd6fab40
sharvani <sharvani2412@gmail.com>
2022-04-23 13:46:25 +0530

    Session2

---
+++ Phase-3/Session2/ExceptionHandling19-04-2022/src/main/java/com/simplilearn/demo/ExceptionHandling19042022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandling19042022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandling19042022Application.class, args);
+	}
+
+}
